3da933d1a3634aeaf75c727d07cc7f8a874d55b0
VitorAndreSilva <alunoitorandre@gmail.com>
2026-07-06 13:07:26 -0300

    FEAT: Adiciona geração de output para saída de dados

--- main.py
+++ main.py
@@ -2,20 +2,36 @@ from leitor import carregar_partidas
 from processador import media_gols
 from validador import validar_partida, separar_registros
 from transformador import transformar_partidas
+import os, csv
 
 # Carregar
 partidas_raw = carregar_partidas("data/matches_original.csv")
 # Validar
 jogos_validos, jogos_invalidos = separar_registros(partidas_raw, validar_partida)
+total_jogos = jogos_validos + jogos_invalidos
 # Processar
 media = media_gols(jogos_validos)
 # Transformar
 partidas = transformar_partidas(jogos_validos)
 
 # Saída
-print(f"jogos válidos: {len(jogos_validos)}")
-print(f"jogos inválidos: {len(jogos_invalidos)}")
+print("===== DATAPROCESSOR - Copa do Mundo 2026 =====")
+print(f"jogos válidos: {len(jogos_validos)}/{len(total_jogos)}")
+print(f"jogos inválidos: {len(jogos_invalidos)}/{len(total_jogos)}")
 print(f"Média de gols: {media}")
 print("Jogos:")
 for jogo in partidas:
     print(jogo)
+
+os.makedirs("output", exist_ok=True)
+caminho_arquivo = os.path.join("output", "partidas_processadas.csv")
+
+if partidas:
+    with open(caminho_arquivo, mode="w", encoding="utf-8") as arquivo_csv:
+        cabecalho = partidas[0]         
+        escritor = csv.DictWriter(arquivo_csv, fieldnames=cabecalho)
+        escritor.writerows(partidas)
+        
+    print(f"\nArquivo salvo com sucesso em: {caminho_arquivo}")
+else:
+    print("\nNenhuma partida válida para salvar.")
